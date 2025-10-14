f577d3b0e5fd700a848f414d4d76e5785d02ea39
Shobi <s.olaadisa@alustudent.com>
2025-10-14 15:11:35 +0200

    Feat: created application entities

---
+++ client/app/src/main/java/com/example/aquaforecast/data/local/AppDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.aquaforecast.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.aquaforecast.data.local.dao.FarmDataDao
+import com.example.aquaforecast.data.local.dao.PondDao
+import com.example.aquaforecast.data.local.dao.PredictionDao
+import com.example.aquaforecast.data.local.entity.FarmDataEntity
+import com.example.aquaforecast.data.local.entity.PondEntity
+import com.example.aquaforecast.data.local.entity.PredictionEntity
+
+@Database(
+    entities = [
+    FarmDataEntity::class,
+    PondEntity::class,
+    PredictionEntity::class
+               ],
+    version = 1
+)
+//@TypeConverters(Converters::class)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun farmDataDao(): FarmDataDao
+    abstract fun pondDao(): PondDao
+    abstract fun predictionDao(): PredictionDao
+}
